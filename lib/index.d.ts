d81da9852a282ddf691668740fd0f5d64e05d4ce
Vince Lwt <vince@lyser.io>
2023-05-15 17:44:39 +0200

    bui;d

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -21,14 +21,63 @@ declare class LLMonitor {
     constructor(options: LLMonitorOptions);
     private trackEvent;
     get id(): string;
-    call(prompt: LLMInput): void;
+    /**
+     * Use this for higher accuracy as soon as the user sends a message.
+     * @param {string} msg - User message
+     **/
+    messageReceived(msg: LLMInput): void;
+    /**
+     * Use this just before calling a model
+     * @param {string | ChatHistory} prompt - Prompt sent to the model
+     **/
+    call(prompt: LLMInput, model?: string): void;
+    /**
+     * Use this when the model returns an answer, but the chain isn't complete yet.
+     * @param {string | ChatHistory} answer - Answer returned by the model
+     **/
     intermediateResult(answer: LLMOutput): void;
+    /**
+     * Use this when the model returns the final answer you'll show to the user.
+     * @param {string | ChatHistory} answer - Answer returned by the model
+     * @example
+     * const answer = await model.generate("Hello")
+     * monitor.finalResult(answer)
+     **/
     finalResult(answer: LLMOutput): void;
+    /**
+     * Use this when the model returns the final answer you'll show to the user.
+     * @param {string | ChatHistory} answer - Answer returned by the model
+     * @example
+     * const answer = await model.generate("Hello")
+     * monitor.result(answer)
+     **/
     result(answer: LLMOutput): void;
     log(message: string): void;
+    /**
+     * Use this when you start streaming the model's output to the user.
+     * Used to measure the time it takes for the model to generate the first response.
+     */
     streamingStarts(): void;
+    /**
+     * Vote on the quality of the conversation.
+     */
     userUpvotes(): void;
+    /**
+     * Vote on the quality of the conversation.
+     */
     userDownvotes(): void;
-    error(message: string | any, error: any): void;
+    /**
+     * Report any errors that occur during the conversation.
+     * @param {string} message - Error message
+     * @param {any} error - Error object
+     * @example
+     * try {
+     *   const answer = await model.generate("Hello")
+     *   monitor.result(answer)
+     * } catch (error) {
+     *   monitor.error("Error generating answer", error)
+     * }
+     **/
+    error(message: string | any, error?: any): void;
 }
 export default LLMonitor;
