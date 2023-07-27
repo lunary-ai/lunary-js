3042e1302d43312c01979034f4c93145a57ae530
hughcrt <me@hugh.sh>
2023-07-27 18:59:00 +0200

    save

---
+++ examples/context/tracer.ts
@@ -0,0 +1,14 @@
+import { AsyncContext } from "./index.ts"
+
+const context = new AsyncContext()
+
+export function log() {
+  const currentId = context.get()
+  if (currentId === undefined)
+    throw new Error("must be inside a run call stack")
+  console.log(`[${currentId}]`, ...arguments)
+}
+
+export function run<T>(id: string, cb: () => T) {
+  context.run(id, cb)
+}
---
+++ examples/test.ts
@@ -0,0 +1,102 @@
+import "dotenv/config"
+
+import { ChatOpenAI } from "langchain/chat_models/openai"
+import { HumanChatMessage, SystemChatMessage } from "langchain/schema"
+
+// import { context } from "./context.ts"
+import { BaseTracer, Run } from "langchain/callbacks"
+import { AsyncContext } from "./context/index.ts"
+
+const context = new AsyncContext("context1")
+class ConsoleCallbackHandler extends BaseTracer {
+  name = "consoleCallbackHandler"
+
+  persistRun(_run: Run) {
+    return Promise.resolve()
+  }
+
+  log() {
+    console.log("LOG")
+  }
+
+  onChainStart(run: Run) {
+    console.log(run)
+  }
+
+  onChainEnd(run: Run) {
+    console.log(run)
+  }
+
+  onChainError(run: Run) {
+    console.log(run)
+  }
+
+  onLLMStart(run: Run) {
+    console.trace("Inside callback handler")
+    console.log(context.get()) // undefined
+    console.log("llm start\n")
+  }
+
+  onLLMEnd(run: Run) {
+    // console.log(context.get(), context)
+    console.log("llm end\n")
+  }
+
+  onLLMError(run: Run) {
+    console.log(run)
+  }
+
+  onToolStart(run: Run) {
+    console.log("tool start", console.log(this))
+  }
+
+  onToolEnd(run: Run) {
+    console.log(run)
+  }
+
+  onToolError(run: Run) {
+    console.log(run)
+  }
+
+  onRetrieverStart(run: Run) {
+    console.log(run)
+  }
+
+  onRetrieverEnd(run: Run) {
+    console.log(run)
+  }
+
+  onRetrieverError(run: Run) {
+    console.log(run)
+  }
+
+  onAgentAction(run: Run) {
+    console.log("agent action")
+  }
+}
+
+const callbackHandler = new ConsoleCallbackHandler()
+
+const chat = new ChatOpenAI({
+  temperature: 0,
+  modelName: "gpt-3.5-turbo",
+  callbacks: [callbackHandler],
+})
+
+async function translate(query: string) {
+  await context.run("123", async () => {
+    console.log(context.get()) // 123
+    console.trace("Outside callback handler")
+    const res = await chat.call([
+      new SystemChatMessage(
+        "You are a translator agent that uses as much slang as possible."
+      ),
+      new HumanChatMessage(
+        `Translate this sentence from English to French: "${query}"`
+      ),
+    ])
+    return res.text
+  })
+}
+
+translate("What's up")
